f05d6afd7868eb357398e44285a45091df65d277
aniedziela96 <niedziela.aleksandra@gmail.com>
2023-05-24 23:43:57 +0200

    rank function

--- hand.py
+++ hand.py
@@ -1,30 +1,134 @@
 from deck import Deck
 
+HAND_RANKS = ['High card', 'Pair', 'Two Pair', 'Three of a kind', 
+              'Straight', 'Flush', 'Full House', 'Four of a kind', 
+           'Straight Flush', 'Royal Flush']
+
 class Hand():
     def __init__(self) -> None:
         self.cards = []
+
 
     def add_cards(self, cards: list) -> None:
         for card in cards:
             self.cards.append(card)
     
+
     def __str__(self) -> str:
         cards_str = ""
         for card in self.cards:
-            cards_str = cards_str + card[0].name + " OF " + card[1].name + ", "
+            if card[0].value == 14:
+                rank = "A"
+            elif card[0].value == 13:
+                rank = "K"
+            elif card[0].value == 12:
+                rank = "Q"
+            elif card[0].value == 11:
+                rank = "J"
+            else:
+                rank = str(card[0].value)
+
+            if card[1].name == "HEARTS":
+                suit = "\N{White Heart Suit}"
+            elif card[1].name == "DIAMONDS":
+                suit = "\N{White Diamond Suit}"
+            elif card[1].name == "CLUBS":
+                suit = "\N{White Club Suit}"
+            elif card[1].name == "SPADES":
+                suit = "\N{White Spade Suit}"
         
-        return cards_str.removesuffix(', ')
+            cards_str = cards_str + rank + suit + ", "
+
+        return cards_str.removesuffix(", ")
     
-    def rank(self) -> str:
-        pass
+
+    def highest_hards(self) -> tuple:
+        return max(self.cards)
+    
+
+    def is_one_color(self) -> bool:
+        for i in range(len(self.cards) - 1):
+            if self.cards[i][1] != self.cards[i + 1][1]:
+                return False
+            
+        return True
+    
+    def is_straight(self) -> bool:
+        sorted_cards = sorted(self.cards)
+        for i in range(len(self.cards) - 1):
+            if sorted_cards[i][0] + 1 != sorted_cards[i + 1][0]:
+                return False
+            
+        return True
+
+
+    def rank(self) -> tuple:
+        if len(self.cards) != 5:
+            raise ValueError ('only possible for 5 cards hands')
+        sorted_hand = sorted(self.cards)
+        score = 0
+        # checks if there is a pair - if there is there's no possibility for straight, colour etc.
+        if sorted_hand[0][0] == sorted_hand[1][0] or \
+           sorted_hand[1][0] == sorted_hand[2][0] or \
+           sorted_hand[2][0] == sorted_hand[3][0] or \
+           sorted_hand[3][0] == sorted_hand[4][0]:
+        # checks if there are two pairs, there are the options
+            if((sorted_hand[0][0] == sorted_hand[1][0] and
+                (sorted_hand[2][0] == sorted_hand[3][0] or
+                sorted_hand[3][0] == sorted_hand [4][0])) or
+                (sorted_hand[1][0] == sorted_hand[2][0] and
+                sorted_hand[3][0] == sorted_hand[4][0])):
+            # checks if there is a three
+                if(sorted_hand[0][0] == sorted_hand[2][0] or
+                   sorted_hand[1][0] == sorted_hand[3][0] or 
+                   sorted_hand[2][0] == sorted_hand[4][0]):
+                # two possible options: full or four
+                # checks for four, if it is not, then we have a full
+                    if(sorted_hand[0][0] == sorted_hand[3][0] or
+                       sorted_hand[1][0] == sorted_hand[4][0]):
+                        return (HAND_RANKS[7], score)
+                    else: 
+                        return(HAND_RANKS[6], score) # Full
+                else:
+                    return(HAND_RANKS[2], score)
+                    # Two Pairs, there's no Three, so it only can be Two Pairs
+            elif (sorted_hand[0][0] == sorted_hand[2][0] or 
+                  sorted_hand[1][0] == sorted_hand[3][0] or 
+                  sorted_hand[2][0] == sorted_hand[4][0]):
+            # Check for Three, but now without the Two Pairs
+                    return(HAND_RANKS[3], score) # Three of a kind
+            # if there is no Three or Two Pairs, there only can be a One Pair
+            else:
+                return(HAND_RANKS[1], score)
+    
+        elif self.is_one_color(): #check for straight, flush
+            if self.is_straight():
+                if sorted_hand[0][0] == 10:
+                    return(HAND_RANKS[9], score) # Royal flush
+                else: 
+                    return(HAND_RANKS[8], score) # Straight flush
+            else:
+                return(HAND_RANKS[5], score) # flush
+        elif self.is_straight():
+            return(HAND_RANKS[4], score) # straight 
+        else: 
+            return(HAND_RANKS[0], score)
+  
 
 if __name__ == "__main__":
-    d = Deck()
-    d.shuffle()
-    h = Hand()
-    c = d.draw(n = 5)
-    print(c)
-    h.add_cards(c)
-    p = str(h)
-    print(p)
-    print(sorted(h.cards))
+    s = set()
+    for i in range(100000):
+        d = Deck()
+        d.shuffle()
+        h = Hand()
+        c = d.draw(n = 5)
+        h.add_cards(c)
+        r = h.rank()
+        if r[0] not in ["Pair", "High card", "Three of a kind", "Two Pair", 
+                        "Straight", 'Flush', 'Full House', "Four of a kind"]:
+            print(h)
+            print(r)
+        
+        s.add(r)
+
+    print(s)
--- tests.py
+++ tests.py
@@ -7,25 +7,40 @@ class TestDeckMethods(unittest.TestCase):
     def setUp(self) -> None:
         self.deck_1 = Deck()
         self.hand_1 = Hand()
+        self.hand_2 = Hand()
+
 
     def test_is_empty(self):
         self.assertFalse(self.deck_1.is_empty())
         Deck.draw(self.deck_1, n = 52)
         self.assertTrue(self.deck_1.is_empty())
 
+
     def test_draw(self): 
         self.assertEqual(self.deck_1.draw(n = 1), [(Rank.ACE, Suit.CLUBS)])
         with self.assertRaises(IndexError):
             Deck.draw(self.deck_1, n = 100)
+
 
     def test_add_cards(self):
         cards_drawn = self.deck_1.draw(6)
         self.hand_1.add_cards(cards_drawn)
         self.assertEqual(self.hand_1.cards, cards_drawn)
 
-    def test_str_hand(self):
+
+    def test_one_color(self):
         self.hand_1.add_cards([(Rank.ACE, Suit.CLUBS), (Rank.ACE, Suit.HEARTS)])
-        self.assertEqual(str(self.hand_1), "ACE OF CLUBS, ACE OF HEARTS")
+        self.hand_2.add_cards(self.deck_1.draw(7))
+        self.assertFalse(self.hand_1.is_one_color())
+        self.assertTrue(self.hand_2.is_one_color())
+        self.hand_2.add_cards([(Rank.ACE, Suit.HEARTS)])
+        self.assertFalse(self.hand_2.is_one_color())
+
+    def test_is_straight(self):
+        self.hand_1.add_cards([(Rank.ACE, Suit.CLUBS), (Rank.ACE, Suit.HEARTS)])
+        self.hand_2.add_cards(self.deck_1.draw(7))
+        self.assertFalse(self.hand_1.is_straight())
+        self.assertTrue(self.hand_2.is_straight())
 
 
 
