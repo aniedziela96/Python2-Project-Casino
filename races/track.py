e4d7a917ad2775cc59cb4ca9257e6cc35a9d0a2b
helseslo <imasnickertv@gmail.com>
2023-06-15 18:20:38 +0200

    added races documentation

--- races/track.py
+++ races/track.py
@@ -1,5 +1,16 @@
 class Track:
+    """"
+    A class used to represent a track for races.
+
+    :param track_type: A track type
+    :type track_type: str
+    :param distance: A distance of the track (in meters)
+    :type distance: int
+    """
     def __init__(self, track_type: str, distance: int) -> None:
+        """
+        Constructor method.
+        """
         self.track_type = track_type
         self.distance = distance
 
@@ -7,7 +18,19 @@ class Track:
         return f"The track is {self.track_type} in length {self.distance} m."
 
     def get_track_type(self) -> str:
+        """
+        Track type's getter.
+
+        :return: The type of the track
+        :rtype: str
+        """
         return self.track_type
 
     def get_distance(self) -> int:
+        """
+        Distance's getter.
+
+        :return: The distance of the track
+        :rtype: int
+        """
         return self.distance
